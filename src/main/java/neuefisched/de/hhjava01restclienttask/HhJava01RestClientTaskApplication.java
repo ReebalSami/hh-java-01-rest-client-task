450f5f13e61fc78de63264defad1241c20c3e8c5
Reebal Sami <reebal.sami@gmail.com>
2024-02-19 15:08:46 +0100

    Setup added

--- src/main/java/neuefisched/de/hhjava01restclienttask/HhJava01RestClientTaskApplication.java
+++ src/main/java/neuefisched/de/hhjava01restclienttask/HhJava01RestClientTaskApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HhJava01RestClientTaskApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HhJava01RestClientTaskApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HhJava01RestClientTaskApplication.class, args);
+    }
 
 }
--- src/test/java/neuefisched/de/hhjava01restclienttask/HhJava01RestClientTaskApplicationTests.java
+++ src/test/java/neuefisched/de/hhjava01restclienttask/HhJava01RestClientTaskApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HhJava01RestClientTaskApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
