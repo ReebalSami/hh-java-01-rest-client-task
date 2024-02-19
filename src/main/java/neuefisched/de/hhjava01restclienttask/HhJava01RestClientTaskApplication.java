1ca1a161a20762a513017dc922b19fadfdc33f4b
Reebal Sami <reebal.sami@gmail.com>
2024-02-19 15:08:11 +0100

    Initial commit

---
+++ src/main/java/neuefisched/de/hhjava01restclienttask/HhJava01RestClientTaskApplication.java
@@ -0,0 +1,13 @@
+package neuefisched.de.hhjava01restclienttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhJava01RestClientTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhJava01RestClientTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/neuefisched/de/hhjava01restclienttask/HhJava01RestClientTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package neuefisched.de.hhjava01restclienttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhJava01RestClientTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
